d354670d8f9163f8b74a0ccd8bc04c9d7032b194
Ursnazar <connect_n@mail.ru>
2022-01-27 15:17:33 +0600

    Second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,13 @@ import Leftbar from "./components/Leftbar";
 import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  /* right: {
+    [theme.breakpoint.down("sm")]: {
+      display: "none",
+    },
+  }, */
+}));
 
 function App() {
   const classes = useStyles();
@@ -15,13 +21,13 @@ function App() {
     <div>
       <Navbar />
       <Grid container>
-        <Grid item sm={2}>
+        <Grid item sm={2} xs={2}>
           <Leftbar />
         </Grid>
-        <Grid item sm={7}>
+        <Grid item sm={7} xs={10}>
           <Feed />
         </Grid>
-        <Grid item sm={3}>
+        <Grid item sm={3} className={classes.right}>
           <Rightbar />
         </Grid>
       </Grid>
--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -3,12 +3,22 @@ import React from "react";
 import { makeStyles } from "@material-ui/core";
 import { Container, Typography } from "@material-ui/core";
 import HomeIcon from "@mui/icons-material/Home";
+import PersonPinIcon from "@mui/icons-material/PersonPin";
+import LinkedCameraIcon from "@mui/icons-material/LinkedCamera";
+import EmailIcon from "@mui/icons-material/Email";
+import Brightness6Icon from "@mui/icons-material/Brightness6";
+import AppShortcutIcon from '@mui/icons-material/AppShortcut';
 
 const useStyles = makeStyles((theme) => ({
   container: {
     height: "100vh",
     paddingTop: theme.spacing(10),
     backgroundColor: theme.palette.primary.main,
+    [theme.breakpoints.up("sm")]: {
+      backgroundColor: "white",
+      color: "#555",
+      border: "1px solid #ece7e7",
+    },
   },
   item: {
     display: "flex",
@@ -19,7 +29,14 @@ const useStyles = makeStyles((theme) => ({
       cursor: "pointer",
     },
   },
+  icon: {
+    marginRight: theme.spacing(1),
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "18px",
+    },
+  },
   text: {
+    fontWeight: 500,
     [theme.breakpoints.down("sm")]: {
       display: "none",
     },
@@ -32,27 +49,27 @@ function Leftbar() {
     <Container className={classes.container}>
       <div className={classes.item}>
         <HomeIcon className={classes.icon} />
-        <Typography className={classes.text}>HomeIconpage</Typography>
+        <Typography className={classes.text}>Home</Typography>
       </div>
       <div className={classes.item}>
-        <HomeIcon className={classes.icon} />
-        <Typography className={classes.text}>HomeIconpage</Typography>
+        <PersonPinIcon className={classes.icon} />
+        <Typography className={classes.text}>Your Profile</Typography>
       </div>
       <div className={classes.item}>
-        <HomeIcon className={classes.icon} />
-        <Typography className={classes.text}>HomeIconpage</Typography>
+        <LinkedCameraIcon className={classes.icon} />
+        <Typography className={classes.text}>Camera</Typography>
       </div>
       <div className={classes.item}>
-        <HomeIcon className={classes.icon} />
-        <Typography className={classes.text}>HomeIconpage</Typography>
+        <EmailIcon className={classes.icon} />
+        <Typography className={classes.text}>Send Email</Typography>
       </div>
       <div className={classes.item}>
-        <HomeIcon className={classes.icon} />
-        <Typography className={classes.text}>HomeIconpage</Typography>
+        <Brightness6Icon className={classes.icon} />
+        <Typography className={classes.text}>Weather</Typography>
       </div>
       <div className={classes.item}>
-        <HomeIcon className={classes.icon} />
-        <Typography className={classes.text}>HomeIconpage</Typography>
+        <AppShortcutIcon className={classes.icon} />
+        <Typography className={classes.text}>Apps</Typography>
       </div>
     </Container>
   );
